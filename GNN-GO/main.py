4a26b8b6fc45834b9b64101d80f99e8ef14c891f
macamadridb <MAMADRID2020@UDEC.CL>
2025-07-25 16:14:16 -0400

    cambio de path

--- GNN-GO/main.py
+++ GNN-GO/main.py
@@ -37,7 +37,7 @@ BASE_INPUT_DIR = os.path.join(os.path.dirname(__file__),"input")
 BASE_OUTPUT_DIR = os.path.join(os.path.dirname(__file__),"output") 
 # Crear paths individuales
 edge_path = os.path.join(BASE_INPUT_DIR, "Edge.csv")
-go_path = os.path.join(BASE_INPUT_DIR, "Go.csv")
+go_path = os.path.join(BASE_INPUT_DIR, "GO.csv")
 protein_metadata_path = os.path.join(BASE_INPUT_DIR, "metadata_proteins.csv")
 go_metadata_path = os.path.join(BASE_INPUT_DIR, "metadata_GO.csv")
 
