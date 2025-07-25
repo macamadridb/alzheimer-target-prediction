a957340a5d561c1fbd09b272f30e1b100fd28b20
macamadridb <MAMADRID2020@UDEC.CL>
2025-07-25 16:17:03 -0400

    cambio de path

--- GNN-GO/analyze_embeddings.py
+++ GNN-GO/analyze_embeddings.py
@@ -15,7 +15,7 @@ GO_ONTOLOGY_FILTER = "all"  # ajustar si aplica filtro
 
 # Crear paths individuales
 edge_path = os.path.join(BASE_INPUT_DIR, "Edge.csv")
-go_path = os.path.join(BASE_INPUT_DIR, "Go.csv")
+go_path = os.path.join(BASE_INPUT_DIR, "GO.csv")
 protein_metadata_path = os.path.join(BASE_INPUT_DIR, "metadata_proteins.csv")
 go_metadata_path = os.path.join(BASE_INPUT_DIR, "metadata_GO.csv")
 
